e2c5ac18d81c0e8b1504dc16cae123095ef8a44e
eric <eric@eMacBook-Pro.local>
2018-07-09 12:35:27 -0700

    make test more DRY

--- tests/test_fox_show_collections_for_dupes.py
+++ tests/test_fox_show_collections_for_dupes.py
@@ -5,6 +5,8 @@ from page_objects import account
 from page_objects import base_page
 from page_objects import shows
 
+OTHER_COLLECTIONS = ['FX', 'National Geographic', 'FOX Sports', 'All Shows']
+
 
 @pytest.mark.nondestructive
 def test_shows_for_dupes_with_existing_account(selenium, base_url):
@@ -23,29 +25,10 @@ def test_shows_for_dupes_with_existing_account(selenium, base_url):
     fox_last_four_shows.sort()
     spreadsheet_utils.write_fox_show(fox_last_four_shows, file=show_dupes_file)
 
-    collection = 'FX'
-    shows.go_to_shows_collection(selenium, collection=collection)
-    fx_shows = shows.get_show_names(selenium)
-    spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=fx_shows,
-                                               collection=collection, file=show_dupes_file)
-
-    collection = 'National Geographic'
-    shows.go_to_shows_collection(selenium, collection=collection)
-    ng_shows = shows.get_show_names(selenium)
-    spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=ng_shows,
-                                               collection=collection, file=show_dupes_file)
-
-    collection = 'FOX Sports'
-    shows.go_to_shows_collection(selenium, collection=collection)
-    fox_sports_shows = shows.get_show_names(selenium)
-    spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=fox_sports_shows,
-                                               collection=collection, file=show_dupes_file)
-
-    collection = 'All Shows'
-    shows.go_to_shows_collection(selenium, collection=collection)
-    all_shows = shows.get_show_names(selenium)
-    spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=all_shows,
-                                               collection=collection, file=show_dupes_file)
+    for collection in OTHER_COLLECTIONS:
+        shows.go_to_shows_collection(selenium, collection=collection)
+        other_shows = shows.get_show_names(selenium)
+        spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=other_shows, collection=collection, file=show_dupes_file)
 
 
 def test_shows_for_dupes_with_new_account(selenium, base_url):
@@ -65,26 +48,7 @@ def test_shows_for_dupes_with_new_account(selenium, base_url):
     fox_last_four_shows.sort()
     spreadsheet_utils.write_fox_show(fox_last_four_shows, file=show_dupes_file)
 
-    collection = 'FX'
-    shows.go_to_shows_collection(selenium, collection=collection)
-    fx_shows = shows.get_show_names(selenium)
-    spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=fx_shows,
-                                               collection=collection, file=show_dupes_file)
-
-    collection = 'National Geographic'
-    shows.go_to_shows_collection(selenium, collection=collection)
-    ng_shows = shows.get_show_names(selenium)
-    spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=ng_shows,
-                                               collection=collection, file=show_dupes_file)
-
-    collection = 'FOX Sports'
-    shows.go_to_shows_collection(selenium, collection=collection)
-    fox_sports_shows = shows.get_show_names(selenium)
-    spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=fox_sports_shows,
-                                               collection=collection, file=show_dupes_file)
-
-    collection = 'All Shows'
-    shows.go_to_shows_collection(selenium, collection=collection)
-    all_shows = shows.get_show_names(selenium)
-    spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=all_shows,
-                                               collection=collection, file=show_dupes_file)
+    for collection in OTHER_COLLECTIONS:
+        shows.go_to_shows_collection(selenium, collection=collection)
+        other_shows = shows.get_show_names(selenium)
+        spreadsheet_utils.write_if_dupe_collection(fox_last_four_shows, collection_shows=other_shows, collection=collection, file=show_dupes_file)
